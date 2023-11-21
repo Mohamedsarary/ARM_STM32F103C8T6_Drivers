d27b245076776aaf8cdeaafd0e71b59bcba7aeb3
Mohamedsarary <Sarary221@gmail.com>
2023-11-21 20:30:44 +0200

    Add LIB File

---
+++ 01_LIB/StdTypes.h
@@ -0,0 +1,30 @@
+/*
+ *  	Created on: 15/03/2023
+ *      Driver: STD_TYPES
+ *      Author: Mohamed sarary
+ *      File  : STD_TYPES.h
+ */
+
+#ifndef SERVICES_STD_TYPES_H_
+#define SERVICES_STD_TYPES_H_
+
+
+/*	Unsigned Type	*/
+typedef unsigned char 	       u8;
+typedef unsigned short int     u16;
+typedef unsigned long int 	   u32;
+typedef unsigned long long     u64;
+
+/*	Signed Type		*/
+typedef signed char 	       s8;
+typedef signed short int       s16;
+typedef signed int             s32;
+typedef signed long long       s64;
+
+/*	Float Type		*/
+typedef float                  f32;
+typedef double                 f64;
+typedef long double            f128;
+
+
+#endif /* SERVICES_STD_TYPES_H_ */
